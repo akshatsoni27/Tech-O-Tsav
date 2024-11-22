a0eb9389fdeb52280ea82e26e9b048ba1e251f07
akshatsoni27 <akshatsoni0427@gmail.com>
2024-11-22 11:50:00 +0530

    Updated buttons,css and images.

--- Tech-o-tsav webpage/script.js
+++ Tech-o-tsav webpage/script.js
@@ -1,10 +1,20 @@
-function redirectToSite() {
+
+// Google Form
+function redirectToSite1() {
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSco39DPJK4gIxwjRDzdz-wQpb-0-6W-2DixclFdkXm5JKpvag/viewform";
 }
-function redirectToSitee() {
+
+// Schedule 
+function redirectToSite2() {
     window.location.href = "https://drive.google.com/file/d/14Ht3l6CPFDbfu5Tz5KlxOGPwpf5RYt3E/view?usp=sharing";
 }
 
-function redirectToSiteee() {
-    window.location.href = " ";
+// Event Details
+function redirectToSite3() {
+    window.location.href = "#";
+}
+
+// 
+function redirectToSite4() {
+    window.location.href = "https://drive.google.com/file/d/1KVScpLzGQN-y7YwGzpkMQHPEJKjI3AhE/view?usp=sharing";
 }
